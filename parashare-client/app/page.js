4a5ecea3622ba4936ee7cd8b6b0a7f563d7a6793
eden10200 <t323052@m.chukyo-u.ac.jp>
2025-07-11 16:44:44 +0900

    edit page.js

--- parashare-client/app/page.js
+++ parashare-client/app/page.js
@@ -4,12 +4,10 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Page() {
-    const [value, setValue] = useState(0);
     const router = useRouter();
 
     useEffect(() => {
         const timer = setTimeout(() => {
-            setValue(1);
             router.push('/1'); // 1秒後に/1ページに移動
         }, 1000);
         return () => clearTimeout(timer);
@@ -17,7 +15,7 @@ export default function Page() {
 
     return (
         <div>
-            <p>{value}</p>
+            <p>立ち上げ中です 自動的に遷移します</p>
         </div>
     );
 }
