f417fa1dc7bd54b09c3f9405d458517e8fb41226
eden10200 <t323052@m.chukyo-u.ac.jp>
2025-07-13 13:29:54 +0900

    start django

---
+++ django_parashare/parashare_app/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.page0, name='home'),  # ルートURLでページ0を表示
+    path('0/', views.page0, name='page0'),
+    path('1/', views.page1, name='page1'),
+    path('2/', views.page2, name='page2'),
+    path('3/', views.page3, name='page3'),
+    path('4/', views.page4, name='page4'),
+    path('5/', views.page5, name='page5'),
+    path('6/', views.page6, name='page6'),
+    path('7/', views.page7, name='page7'),
+    path('8/', views.page8, name='page8'),
+    path('9/', views.page9, name='page9'),
+]
---
+++ django_parashare/parashare_app/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def startup(request):
+    """立ち上げ中画面を表示"""
+    return render(request, 'startup.html')
+
+def standby(request):
+    """スタンバイ画面を表示"""
+    return render(request, 'standby.html')
+
+def login_success(request):
+    """ログイン成功画面を表示"""
+    student_id = request.GET.get('studentID', 't323')
+    context = {
+        'student_id': student_id
+    }
+    return render(request, 'login_success.html', context)
+
+def page0(request):
+    """ページ0 - 立ち上げ中画面"""
+    return render(request, '0.html')
+
+def page1(request):
+    """ページ1 - スタンバイ画面"""
+    return render(request, '1.html')
+
+def page2(request):
+    """ページ2 - ログイン成功画面"""
+    student_id = request.GET.get('studentID', 't323')
+    context = {
+        'student_id': student_id
+    }
+    return render(request, '2.html', context)
+
+def page3(request):
+    """ページ3"""
+    return render(request, '3.html')
+
+def page4(request):
+    """ページ4"""
+    return render(request, '4.html')
+
+def page5(request):
+    """ページ5"""
+    return render(request, '5.html')
+
+def page6(request):
+    """ページ6"""
+    return render(request, '6.html')
+
+def page7(request):
+    """ページ7"""
+    return render(request, '7.html')
+
+def page8(request):
+    """ページ8"""
+    return render(request, '8.html')
+
+def page9(request):
+    """ページ9"""
+    return render(request, '9.html')
