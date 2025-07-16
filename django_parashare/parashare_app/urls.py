ea57b43dec767a29ddac97ee1a66e7dbd99a3656
eden10200 <t323052@m.chukyo-u.ac.jp>
2025-07-17 00:02:07 +0900

    add nfcsend

--- django_parashare/parashare_app/urls.py
+++ django_parashare/parashare_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from .nfc_reader import read_nfc_tag
+from .nfc_send import send_server_nfc
 
 urlpatterns = [
     path('', views.page0, name='home'),  # ルートURLでページ0を表示
@@ -15,4 +16,5 @@ urlpatterns = [
     path('8/', views.page8, name='page8'),
     path('9/', views.page9, name='page9'),
     path('api/nfc-read/', read_nfc_tag, name='nfc_read'),  # NFC読み取りAPI
+    path('api/nfc-request/', send_server_nfc, name='nfc_read'),  # NFC読み取りAPI
 ]
