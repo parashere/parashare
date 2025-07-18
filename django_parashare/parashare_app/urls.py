9de7d8324f704c1ce1b942a9a5d520e3f3c25e8c
eden10200 <mono.black1020@gmail.com>
2025-07-18 18:42:05 +0900

    edit 1

--- django_parashare/parashare_app/urls.py
+++ django_parashare/parashare_app/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('api/nfc-read/', read_nfc_tag, name='nfc_read'),  # NFC読み取りAPI
     path('api/nfc-request/', send_server_nfc, name='nfc_request'),  # NFCリクエストAPI
     
-    path('/api/servo/open/', views.open_gate_api, name='open_gate_api'),  # ゲートを開くAPI
-    path('/api/servo/close/', views.close_gate_api, name='close_gate_api'),  # ゲートを閉じるAPI    
-    path('/api/servo/status/', views.get_gate_status_api, name='get_gate_status_api'),  # ゲートの状態を取得するAPI    
+    #path('/api/servo/open/', views.open_gate_api, name='open_gate_api'),  # ゲートを開くAPI
+    #path('/api/servo/close/', views.close_gate_api, name='close_gate_api'),  # ゲートを閉じるAPI    
+    #path('/api/servo/status/', views.get_gate_status_api, name='get_gate_status_api'),  # ゲートの状態を取得するAPI    
 ]
