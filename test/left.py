239667337a666038d7064c9bd5d7d3396677158d
eden10200 <mono.black1020@gmail.com>
2025-07-19 16:12:44 +0900

    add

--- test/left.py
+++ test/left.py
@@ -1,9 +1,8 @@
 from gpiozero import Servo
 from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
-
-servo = Servo(13)  # GPIO13に接続
-
-print("value=(val=-1.0)")
-servo.value = 1.0  # 最小位置
+factory = PiGPIOFactory()
+servo = Servo(13, pin_factory=factory)  # GPIO13に接続
+print("value=(val=1.0)")
+servo.value = 1.0  # 最大位置
 sleep(2)
